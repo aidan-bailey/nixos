e6652e3911b9ac19336bd21d794e5b594e262f36
Aidan Bailey <git@aidanbailey.me>
2024-11-12 10:26:22 +0200

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -103,10 +103,9 @@ in
   # SYS CONFIG #
   ##############
 
-  system.stateVersion = "23.05"; # Did you read the comment?
+  system.stateVersion = "23.05";
 
   imports = [
-    # Include the results of the hardware scan.
     ./hardware-configuration.nix
     <home-manager/nixos>
   ];
@@ -215,12 +214,7 @@ in
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
     #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
   };
 
   # RDP
