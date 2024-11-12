18131d9964b0440255bc7867df7ae624c5af27e7
Aidan Bailey <git@aidanbailey.me>
2024-11-12 17:13:29 +0200

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -3,8 +3,7 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
   };
   outputs = { self, nixpkgs }: {
-    # replace 'joes-desktop' with your hostname here.
-    nixosConfigurations.joes-desktop = nixpkgs.lib.nixosSystem {
+    nixosConfigurations.fresco = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [ ./configuration.nix ];
     };
