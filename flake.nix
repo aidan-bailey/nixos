c5515c566886bf340ec17e423f602cb387078268
Aidan Bailey <dev@aidanbailey.me>
2025-10-27 13:47:18 +0200

    Update flake files

--- flake.nix
+++ flake.nix
@@ -5,6 +5,9 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     chaotic.url = "github:chaotic-cx/nyx/nyxpkgs-unstable";
     doom-flake.url = "path:./flakes/doom-emacs";
+    nixarr.url = "github:rasmus-kirk/nixarr";
+    ccache-flake.url = "path:./flakes/ccache";
+    #gcc-lto-pgo.url = "path:./flakes/gcc-lto-pgo";
   };
 
   outputs =
@@ -12,20 +15,28 @@
       self,
       nixpkgs,
       doom-flake,
+      ccache-flake,
       chaotic,
+      nixarr,
+      #gcc-lto-pgo,
       ...
     }@inputs:
     let
       system = "x86_64-linux";
-      pkgs = import nixpkgs { inherit system; };
+      pkgs = import nixpkgs {
+        inherit system;
+      };
     in
     {
       nixosConfigurations.nesco = nixpkgs.lib.nixosSystem {
         inherit system;
         modules = [
           ./hosts/nesco/configuration.nix
+          ccache-flake.nixosModules.ccache
           doom-flake.nixosModules.default
           chaotic.nixosModules.default
+          nixarr.nixosModules.default
+          #gcc-lto-pgo.nixosModules.default
           {
             nixpkgs.config.allowUnfree = true;
           }
