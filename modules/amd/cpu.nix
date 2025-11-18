a37d8a3b526373ac2dba3382580b637f88370791
Aidan Bailey <dev@aidanbailey.me>
2025-11-18 14:45:57 +0200

    Fix hibernation bug

--- modules/amd/cpu.nix
+++ modules/amd/cpu.nix
@@ -12,6 +12,9 @@
     "amd_pstate=active"
     "amdgpu.dc=1"
     "amdgpu.ppfeaturemask=0xffffffff"
+    "amdgpu.dcdebugmask=0x600"
+    "amdgpu.sg_display=0"
+    "amdgpu.abmlevel=0"
   ];
 
   hardware.firmware = with pkgs; [ linux-firmware ];
