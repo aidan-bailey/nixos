2b028c5ef2366a2355f62a5973d5514a825ffe19
Aidan Bailey <dev@aidanbailey.me>
2026-02-26 13:38:13 +0200

    feat: Override Cockatrice package to use specific GitHub source and version

--- home/modules/gaming.nix
+++ home/modules/gaming.nix
@@ -5,7 +5,14 @@
   # Steam system configuration is in modules/gaming.nix
   home.packages = with pkgs; [
     steam
-    cockatrice
+    (cockatrice.overrideAttrs (oldAttrs: {
+      src = pkgs.fetchFromGitHub {
+        owner = "Cockatrice";
+        repo = "Cockatrice";
+        rev = "2026-02-22-Release-2.10.3";
+        hash = "sha256-GQVdn6vUW0B9vSk7ZvSDqMNhLNe86C+/gE1n6wfQIMw=";
+      };
+    }))
   ];
 }
 
