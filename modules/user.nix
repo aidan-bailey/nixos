596136f6901c6ab5a497712d6d90f6a790e2938d
Aidan Bailey <dev@aidanbailey.me>
2026-05-28 16:44:46 +0200

    chore(user): authorize medesco ssh key

--- modules/user.nix
+++ modules/user.nix
@@ -33,6 +33,7 @@
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHEbjAttdt+o26cZKZdfec8Bm1xuuE/2ToNXozF9PIgS aidanb@fresco"
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOX/kOPgPyOn9iJ5YhPK9+F2Ek9YaYqvrA6k2Ki+ALQ1 aidanb@nesco"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIF6F1DZTs0RmjbZXqd4Ro+uDMMlihOtkU3Tk12BGpwIN aidanb@medesco"
     ];
   };
 
